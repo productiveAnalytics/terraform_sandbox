6c682be39b5f0ec6461a13b54979bc774afa8bb5
Chawathe, Lalit <lalit.chawathe@changehealthcare.com>
2019-05-23 09:41:27 -0500

    main terraform script for EC2

---
+++ ec2/main.tf
@@ -0,0 +1,53 @@
+##################################################################################
+# VARIABLES
+##################################################################################
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {
+  default = "HDS_EC2WebServer"
+}
+variable "ec2_AMI" {
+  default = "ami-000196f0f667a5a97"
+}
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-east-1"
+}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+resource "aws_instance" "nginx" {
+  ami           = "${var.ec2_AMI}"
+  instance_type = "t2.micro"
+  key_name      = "${var.key_name}"
+
+  connection {
+    user        = "ec2-user"
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo service nginx start"
+    ]
+  }
+}
+
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "aws_instance_public_dns" {
+    value = "${aws_instance.nginx.public_dns}"
+}
